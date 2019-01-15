2269187164165548dcde4b6530ccfa2e925400e9
gusta <gusta@DESKTOP-50JDIFF>
2019-01-15 21:18:38 -0200

    Lesson 4

--- src/test/java/br/com/gustavo/steps/InsertAccountSteps.java
+++ src/test/java/br/com/gustavo/steps/InsertAccountSteps.java
@@ -18,10 +18,9 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-
 public class InsertAccountSteps {
 	protected static WebDriver driver;
-	
+
 	@Given("^I'm accessing the application$")
 	public void iMAccessingTheApplication() throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
@@ -34,7 +33,7 @@ public class InsertAccountSteps {
 
 	@When("^the user \"([^\"]*)\"$")
 	public void theUser(String arg1) throws Throwable {
-	  driver.findElement(By.id("email")).sendKeys(arg1);
+		driver.findElement(By.id("email")).sendKeys(arg1);
 	}
 
 	@When("^the password \"([^\"]*)\"$")
@@ -71,52 +70,52 @@ public class InsertAccountSteps {
 	@When("^I select Save$")
 	public void iSelectSave() throws Throwable {
 		driver.findElement(By.tagName("button")).click();
-	   
+
 	}
 
 	@Then("^the account is inserted successfully$")
 	public void theAccountIsInsertedSuccessfully() throws Throwable {
 		String text = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
 		Assert.assertEquals("Conta adicionada com sucesso!", text);
-		
+
 	}
-	
+
 	@Then("^I'm notifying the account name is required$")
 	public void iMNotifyingTheAccountNameIsRequired() throws Throwable {
 		String text = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
 		Assert.assertEquals("Informe o nome da conta", text);
-		
+
 	}
-	
+
 	@Then("^I'm notified that there is already an account with this name$")
 	public void iMNotifiedThatThereIsAlreadyAnAccountWithThisName() throws Throwable {
 		String text = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
 		Assert.assertEquals("Já existe uma conta com esse nome!", text);
-		
+
 	}
-	
+
 	@Then("^I get the message \"([^\"]*)\"$")
 	public void iGetTheMessage(String arg1) throws Throwable {
 		String text = driver.findElement(By.xpath("//div[starts-with(@class, 'alert alert-')]")).getText();
 		Assert.assertEquals(arg1, text);
 	}
 
-	@After (order = 1, value = {"@functional"})
-	public void screenshot(Scenario  scenarios){
-		File file=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	@After(order = 1, value = { "@functional" })
+	public void screenshot(Scenario scenarios) {
+		File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		try {
-			FileUtils.copyFile(file,new File("target/screenshot/"+scenarios.getId()+".jpg"));
+			FileUtils.copyFile(file, new File("target/screenshot/" + scenarios.getId() + ".jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	@After(order = 0, value = {"@functional"})
-	public void closeDriver(){
-		
+
+	@After(order = 0, value = { "@functional" })
+	public void closeDriver() {
+
 		driver.close();
 		driver.quit();
-		
+
 	}
 }
